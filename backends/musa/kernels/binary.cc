4dbc267a08f80b26fcf5a695567c20f80e98aa76
maruko <maruko>
2024-04-12 08:09:37 +0000

    support multiply divide equal not_equal operator

--- backends/musa/kernels/binary.cc
+++ backends/musa/kernels/binary.cc
@@ -19,37 +19,60 @@ namespace custom_kernel {
   using UNARY_MODE = ::musa::dnn::Unary::Mode;
 
   template <typename T>
-  void AddKernel(const phi::Context& dev_ctx,
+  void Binary_kernel(const phi::Context& dev_ctx,
                 const phi::DenseTensor& x,
                 const phi::DenseTensor& y,
-                phi::DenseTensor* out) {
+                phi::DenseTensor* out,
+                const BINARY_MODE & m){
       auto out_data = dev_ctx.template Alloc<T>(out);
-
       musa::muHandle& h = musa::GetMudnnHandle();
       muTensor musa_self = CreateMUTensor(x);
       muTensor musa_other = CreateMUTensor(y);
       muTensor musa_out = CreateMUTensor(*out);
-      BINARY_MODE m = BINARY_MODE::ADD;
       ::musa::dnn::Binary bop;
       CHECK_MUDNN_STATUS(bop.SetMode(m),"SetMode");
       CHECK_MUDNN_STATUS(bop.Run(h, musa_out, musa_self, musa_other),"Run");
   }
 
-  // template <typename T>
-  // void MultiplyKernel(const phi::Context& dev_ctx,
-  //                     const phi::DenseTensor& x,
-  //                     const phi::DenseTensor& y,
-  //                     phi::DenseTensor* out) {
-  //   auto out_data = dev_ctx.template Alloc<T>(out);
-  //   musa::muHandle& h = musa::GetMudnnHandle();
-  //   muTensor musa_self = CreateMUTensor(x);
-  //   muTensor musa_other = CreateMUTensor(y);
-  //   muTensor musa_out = CreateMUTensor(*out);
-  //   BINARY_MODE m = BINARY_MODE::MUL;
-  //   ::musa::dnn::Binary bop;
-  //   CHECK_MUDNN_STATUS(bop.SetMode(m),"SetMode");
-  //   CHECK_MUDNN_STATUS(bop.Run(h, musa_out, musa_self, musa_other),"Run");
-// }
+  template <typename T>
+  void AddKernel(const phi::Context& dev_ctx,
+                const phi::DenseTensor& x,
+                const phi::DenseTensor& y,
+                phi::DenseTensor* out) {
+    Binary_kernel<T>(dev_ctx,x,y,out,BINARY_MODE::ADD);
+  }
+
+  template <typename T>
+  void MultiplyKernel(const phi::Context& dev_ctx,
+                      const phi::DenseTensor& x,
+                      const phi::DenseTensor& y,
+                      phi::DenseTensor* out) {
+    Binary_kernel<T>(dev_ctx,x,y,out,BINARY_MODE::MUL);
+  }
+
+  template <typename T>
+  void DivideKernel(const phi::Context& dev_ctx,
+                      const phi::DenseTensor& x,
+                      const phi::DenseTensor& y,
+                      phi::DenseTensor* out) {
+    Binary_kernel<T>(dev_ctx,x,y,out,BINARY_MODE::DIV);
+  }
+
+  template <typename T>
+  void EqualKernel(const phi::Context& dev_ctx,
+                      const phi::DenseTensor& x,
+                      const phi::DenseTensor& y,
+                      phi::DenseTensor* out) {
+    Binary_kernel<bool>(dev_ctx,x,y,out,BINARY_MODE::EQ);
+  }
+
+  template <typename T>
+  void NotEqualKernel(const phi::Context& dev_ctx,
+                      const phi::DenseTensor& x,
+                      const phi::DenseTensor& y,
+                      phi::DenseTensor* out) {
+    Binary_kernel<bool>(dev_ctx,x,y,out,BINARY_MODE::NE);
+  }
 
 }  // namespace custom_kernel
 
@@ -63,3 +86,39 @@ PD_BUILD_PHI_KERNEL(add,
                     int64_t,
                     float,
                     double) {}
+
+PD_BUILD_PHI_KERNEL(multiply,
+                    musa,
+                    ALL_LAYOUT,
+                    custom_kernel::MultiplyKernel,
+                    int32_t,
+                    int64_t,
+                    float,
+                    double) {}
+
+PD_BUILD_PHI_KERNEL(divide,
+                    musa,
+                    ALL_LAYOUT,
+                    custom_kernel::DivideKernel,
+                    int32_t,
+                    int64_t,
+                    float,
+                    double) {}
+
+PD_BUILD_PHI_KERNEL(equal,
+                    musa,
+                    ALL_LAYOUT,
+                    custom_kernel::EqualKernel,
+                    int32_t,
+                    int64_t,
+                    float,
+                    double) {}
+
+PD_BUILD_PHI_KERNEL(not_equal,
+                    musa,
+                    ALL_LAYOUT,
+                    custom_kernel::NotEqualKernel,
+                    int32_t,
+                    int64_t,
+                    float,
+                    double) {}                  
